62ca9463905891fd57f61eecf62b548d9f241c0d
Felipe Rodríguez <feliperodriguez@MacBook-Pro-de-Felipe.local>
2019-08-24 09:44:37 -0400

    Search component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import './App.scss';
 import axios from 'axios';
+import Item from './components/Item';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      items: []
-    }
+      query: '',
+      items: [],
+      data: null
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangeInput = this.handleChangeInput.bind(this);
   }
   /*
     Actividad Nº 2
@@ -24,17 +30,58 @@ class App extends Component {
     - Agregar dentro del .nav-header el logo (https://http2.mlstatic.com/ui/navigation/4.5.0/mercadolibre/logo__large_plus@2x.png)
   */
 
+  handleSubmit(event) {
+    axios
+      .get(
+        `https://api.mercadolibre.com/sites/MLC/search?q=${this.state.query}`,
+        this.state
+      )
+      .then(response => {
+        console.info(response);
+        console.info(response.data);
+        if (response.status === 200) {
+          this.setState({ items: response.data.results });
+        }
+      });
+
+    event.preventDefault();
+  }
+
+  handleChangeInput(event) {
+    this.setState({ query: event.target.value });
+  }
+
   render() {
-    const {items} = this.state;
+    const { query, items } = this.state;
     return (
       <div className="App">
         <div className="nav-header">
-          { /* Aqui formulario buscador */ }
+          <img
+            className="nav-header--logo"
+            src="https://http2.mlstatic.com/ui/navigation/4.5.0/mercadolibre/logo__large_plus@2x.png"
+            alt="Logo Mercado Libre"
+          />
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              value={query}
+              placeholder="Ej: iPhone"
+              onChange={this.handleChangeInput}
+            />
+            <input type="submit" value="Buscar" />
+          </form>
         </div>
-        
-
-        {/* items.map(item => (<TuComponenteFila />)) */}
-        
+        <ol className="results">
+          {items.map(item => (
+            <Item
+              key={item.id}
+              title={item.title}
+              img={item.thumbnail}
+              price={item.price}
+              // oldPrice={item.original_price}
+            />
+          ))}
+        </ol>
       </div>
     );
   }
