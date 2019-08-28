014448596162ff399ad9484c939e1210ba6860a2
Felipe Rodríguez <felipe.rodriguezn@gmail.com>
2019-08-28 11:50:04 -0400

    Add React Router and React CSS Transition Group

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Detail from './components/Detail';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppRouter = () => {
+  return (
+    <Router>
+      <Route component={App} path="/" exact />
+      <Route component={Detail} path="/detail/:id" />
+    </Router>
+  );
+};
+
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
